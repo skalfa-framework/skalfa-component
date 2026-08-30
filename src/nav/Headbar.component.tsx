1c86b792ba79b686a22fe406a8471b5e7301ae32
DarkLocuts <darklocuts23@gmail.com>
2026-08-30 08:41:31 +0700

    feat: add Headbar component and bump package version to 1.0.45

--- src/nav/Headbar.component.tsx
+++ src/nav/Headbar.component.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useToggleContext } from "@contexts";
 import { Icon, type IconName } from "@skalfa/skalfa-icon";
 
 
@@ -11,18 +12,25 @@ export interface HeadbarProps {
   title         ?:  string;
   backHref      ?:  string | boolean;
   rightContent  ?:  ReactNode;
+  sidebarId     ?:  string;
 };
 
 
-export function HeadbarComponent({ title, backHref, rightContent }: HeadbarProps) {
+export function HeadbarComponent({ title, backHref, rightContent, sidebarId }: HeadbarProps) {
   const router  =  useRouter();
+  const { toggle, setToggle }  =  useToggleContext()
 
   return (
     <div className="flex justify-between w-full items-center mt-2 mb-4">
       <div className="flex items-center">
+        {toggle[`SIDEBAR${sidebarId ? ("_" + sidebarId?.toUpperCase()) : ""}`] && (
+          <div className="w-8 aspect-square flex justify-center items-center cursor-pointer " onClick={() => setToggle(`SIDEBAR${sidebarId ? ("_" + sidebarId?.toUpperCase()) : ""}`, !toggle[`SIDEBAR${sidebarId ? ("_" + sidebarId?.toUpperCase()) : ""}`])}>
+            <Icon icon="solid/sidebar" className="text-xl" />
+          </div>
+        )}
         {backHref && (
           <div className="w-8 aspect-square flex justify-center items-center cursor-pointer" onClick={() => typeof backHref != "boolean" ? router.push(backHref) : router.back()}>
-            <Icon icon="solid/chevron-left" className="text-secondary text-xl" />
+            <Icon icon="solid/chevron-left" className="text-xl" />
           </div>
         )}
         <p className="text-lg font-bold px-2">{title}</p>
